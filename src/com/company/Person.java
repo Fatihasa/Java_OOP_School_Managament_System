aa6ab65b10e85eadee5c107863374fcb548efc86
Fatih_ASA <fatihasa96@gmail.com>
2020-08-04 11:56:33 +0200

    Add .gitingnore file ingoring all IDE setting files

---
+++ src/com/company/Person.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public abstract class Person {
+
+    String name;
+    String surname;
+    String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
